e46eaa08bb6bf4d3448d27e08e6df24f206575e5
musa <musagillaniwork@gmail.com>
2022-02-18 12:37:26 +0500

    added cors headers print server port number unintalled pg-hstore package

--- Backend/app.js
+++ Backend/app.js
@@ -7,6 +7,24 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// CORS Headers Middleware
+app.use((req, res, next) => {
+  // note: setHeader does not send a response, json() and render() did
+  // second argument is the server(domain) we want to set this header for
+  // in this case the * wildcard sets this header for all servers(domains)
+  // multiple domains can be entered by using ',' comma as a separator
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  // above we are allowing "Origins" to access data
+
+  // telling which methods are allowed
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+
+  // allowing the type of headers the client can set
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
+
+  next();
+});
+
 // Setup a default catch-all route that sends back a welcome message in JSON format.
 // Require our routes into the application.
 require("./server/routes")(app);
