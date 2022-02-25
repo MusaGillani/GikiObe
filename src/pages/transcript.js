40d2b3d144b294f46ef730c6498774f92dd0a185
hr_hassan_raza <hraza20000@gmail.com>
2022-02-26 02:41:36 +0500

    New-Json-Format-Dynamic

--- src/pages/transcript.js
+++ src/pages/transcript.js
@@ -13,6 +13,11 @@ export const Transcript = React.forwardRef((props, ref) => {
   const [batch, setBatch] = useState("");
   //const [flag, setFlag] = useState(1);
 
+  function yesNo(string) {
+    if (string) return string.charAt(0).toUpperCase();
+    else return "-";
+  }
+
   if (props.reg) {
     //console.log(props.reg);
     setRegNum(props.reg);
@@ -28,7 +33,13 @@ export const Transcript = React.forwardRef((props, ref) => {
         delete data["name"];
         delete data["batch"];
         delete data["reg"];
-        setDetails(data);
+        const ordered = Object.keys(data["result"])
+          .sort()
+          .reduce((obj, key) => {
+            obj[key] = data["result"][key];
+            return obj;
+          }, {});
+        setDetails(ordered);
       });
   }, [regNo]);
   return (
@@ -36,10 +47,22 @@ export const Transcript = React.forwardRef((props, ref) => {
     //   {details && (
     //     <div>
     //       {Object.keys(details).map((row) => {
-    //           Object.keys(details[row]).map((year) => (
-    //             <div>{console.log(year)}</div>
-    //           ));
+    //         const ordered = Object.keys(details[row])
+    //           .sort()
+    //           .reduce((obj, key) => {
+    //             obj[key] = details[row][key];
+    //             return obj;
+    //           }, {});
+    //         //console.log(ordered);
+    //         return (
+    //           <div>
+    //             {Object.keys(ordered).map((year) => (
+    //               <div>Hello World</div>
+    //             ))}
+    //           </div>
+    //         );
     //       })}
+    //       {/* {console.log(details["2018"])} */}
     //     </div>
     //   )}
     // </div>
@@ -69,79 +92,95 @@ export const Transcript = React.forwardRef((props, ref) => {
           </p>
         </div>
       </div>
-      {Object.keys(details).map((row_r) => (
-        <div>
-          {Object.keys(details[row_r]).map((year) => (
-            //let year = year_no.split("_")[1];
-            <div class="w-100">
-              <table class="table table-bordered">
-                <thead>
-                  <tr>
-                    <th
-                      class="border-start-1 border-end-0 border-top-0 fw-normal"
-                      scope="col"
-                      style={{ width: "20%" }}
-                    >
-                      {year}
-                    </th>
-                    <th
-                      class="border-0 border-top-0"
-                      scope="col"
-                      style={{ width: "25%" }}
-                    ></th>
-                    <th
-                      class="table-light rowhead"
-                      scope="col"
-                      colspan="12"
-                      style={{ width: "55%" }}
-                    >
-                      PLOs
-                    </th>
-                  </tr>
-                </thead>
-                <thead>
-                  <tr>
-                    <th scope="col">Course Code</th>
-                    <th scope="col">Course Title</th>
-                    <th scope="col">1</th>
-                    <th scope="col">2</th>
-                    <th scope="col">3</th>
-                    <th scope="col">4</th>
-                    <th scope="col">5</th>
-                    <th scope="col">6</th>
-                    <th scope="col">7</th>
-                    <th scope="col">8</th>
-                    <th scope="col">9</th>
-                    <th scope="col">10</th>
-                    <th scope="col">11</th>
-                    <th scope="col">12</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {details[row_r][year].map((row) => (
-                    <tr>
-                      <td scope="row">{row.course_code}</td>
-                      <td scope="row">{row.course_name}</td>
-                      <td scope="row">{row.PLO1 == "" ? "-" : row.PLO1}</td>
-                      <td scope="row">{row.PLO2 == "" ? "-" : row.PLO2}</td>
-                      <td scope="row">{row.PLO3 == "" ? "-" : row.PLO3}</td>
-                      <td scope="row">{row.PLO4 == "" ? "-" : row.PLO4}</td>
-                      <td scope="row">{row.PLO5 == "" ? "-" : row.PLO5}</td>
-                      <td scope="row">{row.PLO6 == "" ? "-" : row.PLO6}</td>
-                      <td scope="row">{row.PLO7 == "" ? "-" : row.PLO7}</td>
-                      <td scope="row">{row.PLO8 == "" ? "-" : row.PLO8}</td>
-                      <td scope="row">{row.PLO9 == "" ? "-" : row.PLO9}</td>
-                      <td scope="row">{row.PLO10 == "" ? "-" : row.PLO10}</td>
-                      <td scope="row">{row.PLO11 == "" ? "-" : row.PLO11}</td>
-                      <td scope="row">{row.PLO12 == "" ? "-" : row.PLO12}</td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
+      <div>
+        {Object.keys(details).map((row_r) => {
+          const ordered = Object.keys(details[row_r])
+            .sort()
+            .reduce((obj, key) => {
+              obj[key] = details[row_r][key];
+              return obj;
+            }, {});
+          return (
+            <div>
+              {Object.keys(ordered).map((year) => (
+                <div>
+                  <div class="w-100">
+                    <table class="table table-bordered">
+                      <thead>
+                        <tr>
+                          <th
+                            class="border-start-1 border-end-0 border-top-0 fw-normal"
+                            scope="col"
+                            style={{ width: "20%" }}
+                          >
+                            {(() => {
+                              if (year == 3) return <div>Fall {row_r}</div>;
+                              else if (year == 2)
+                                return <div>Summer {row_r}</div>;
+                              else return <div>Spring {row_r}</div>;
+                            })()}
+                          </th>
+                          <th
+                            class="border-0 border-top-0"
+                            scope="col"
+                            style={{ width: "25%" }}
+                          ></th>
+                          <th
+                            class="table-light rowhead"
+                            scope="col"
+                            colspan="12"
+                            style={{ width: "55%" }}
+                          >
+                            PLOs
+                          </th>
+                        </tr>
+                      </thead>
+                      <thead>
+                        <tr>
+                          <th scope="col">Course Code</th>
+                          <th scope="col">Course Title</th>
+                          <th scope="col">1</th>
+                          <th scope="col">2</th>
+                          <th scope="col">3</th>
+                          <th scope="col">4</th>
+                          <th scope="col">5</th>
+                          <th scope="col">6</th>
+                          <th scope="col">7</th>
+                          <th scope="col">8</th>
+                          <th scope="col">9</th>
+                          <th scope="col">10</th>
+                          <th scope="col">11</th>
+                          <th scope="col">12</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {details[row_r][year].map((row) => (
+                          <tr>
+                            <td scope="row">{row.CourseCode}</td>
+                            <td scope="row">{row.courseTitle}</td>
+                            <td scope="row">{yesNo(row.PLO1)}</td>
+                            <td scope="row">{yesNo(row.PLO2)}</td>
+                            <td scope="row">{yesNo(row.PLO3)}</td>
+                            <td scope="row">{yesNo(row.PLO4)}</td>
+                            <td scope="row">{yesNo(row.PLO5)}</td>
+                            <td scope="row">{yesNo(row.PLO6)}</td>
+                            <td scope="row">{yesNo(row.PLO7)}</td>
+                            <td scope="row">{yesNo(row.PLO8)}</td>
+                            <td scope="row">{yesNo(row.PLO9)}</td>
+                            <td scope="row">{yesNo(row.PLO10)}</td>
+                            <td scope="row">{yesNo(row.PLO11)}</td>
+                            <td scope="row">{yesNo(row.PLO12)}</td>
+                          </tr>
+                        ))}
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              ))}
             </div>
-          ))}
-        </div>
-      ))}
+          );
+        })}
+      </div>
     </div>
   );
 });
