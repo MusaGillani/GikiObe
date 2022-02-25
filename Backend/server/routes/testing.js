dd535827907df513716ed7edec0fda375444d2da
Muhammad Mohsin Zafar <m.mohsin.zafar@outlook.com>
2022-02-25 22:31:09 +0500

    Transcript API - Sorting Bug Fix - 1

--- Backend/server/routes/testing.js
+++ Backend/server/routes/testing.js
@@ -1,273 +1,218 @@
-const { PrismaClient } = require("@prisma/client");
+const {PrismaClient} = require("@prisma/client");
 
 const prisma = new PrismaClient();
 const router = require("express").Router();
 
-router.get("/student", async (req, res, next) => {
-  const users = await prisma.student.findMany();
+const semesterLookup = {
+    "Spring": 1,
+    "Summer": 2,
+    "Fall": 3,
+}
 
-  res.send(JSON.stringify(users));
+router.get("/student", async (req, res, next) => {
+    const users = await prisma.student.findMany();
+
+    res.send(JSON.stringify(users));
 });
 router.post("/student", async (req, res, next) => {
-  // run inside `async` function
-  try {
-    const newUser = {
-      RegNo: req.body.reg,
-      Name: req.body.name,
-      Faculty: req.body.faculty,
-      Batch: req.body.batch,
-    };
-    await prisma.student.create({
-      data: newUser,
-    });
+    // run inside `async` function
+    try {
+        const newUser = {
+            RegNo: req.body.reg,
+            Name: req.body.name,
+            Faculty: req.body.faculty,
+            Batch: req.body.batch,
+        };
+        await prisma.student.create({
+            data: newUser,
+        });
 
-    res.status(200).send({ message: "Added!" });
-  } catch (e) {
-    res.status(404).send(JSON.stringify(e));
-  }
+        res.status(200).send({message: "Added!"});
+    } catch (e) {
+        res.status(404).send(JSON.stringify(e));
+    }
 });
 
 router.get("/courseplo", async (req, res, next) => {
-  const users = await prisma.courseplo.findMany();
+    const users = await prisma.courseplo.findMany();
 
-  res.send(JSON.stringify(users));
+    res.send(JSON.stringify(users));
 });
 
 router.get("/scheme", async (req, res, next) => {
-  const users = await prisma.schemeofstudy.findMany();
+    const users = await prisma.schemeofstudy.findMany();
 
-  res.send(JSON.stringify(users));
+    res.send(JSON.stringify(users));
 });
 
 router.get("/transcript/:reg", async (req, res, next) => {
-  try {
-    const data = {
-      reg: parseInt(req.params.reg),
-      //   batch: req.query.batch,
-    };
+    try {
+        const data = {
+            reg: parseInt(req.params.reg),
+            //   batch: req.query.batch,
+        };
 
-    const student = await prisma.student.findUnique({
-      where: {
-        RegNo: data.reg,
-      },
-    });
-
-    const schemeCourses = await prisma.schemeofstudy.findMany({
-      where: {
-        Batch: student.batch,
-      },
-      select: {
-        Semester: true,
-        CourseCode: true,
-      },
-      orderBy: {
-        Semester: "asc",
-      },
-    });
-
-    // console.log(schemeCourses);
-
-    let codes = [];
-    schemeCourses.forEach((course) => {
-      //   console.log(code);
-      codes.push(course.CourseCode);
-    });
-
-    // console.log("codes", codes);
-
-    let result = await prisma.courseplo.findMany({
-      select: {
-        Semester: true,
-        // CourseTitle: true,
-        CourseCode: true,
-        PLO1: true,
-        PLO2: true,
-        PLO3: true,
-        PLO4: true,
-        PLO5: true,
-        PLO6: true,
-        PLO7: true,
-        PLO8: true,
-        PLO9: true,
-        PLO10: true,
-        PLO11: true,
-        PLO12: true,
-      },
-      where: {
-        AND: [
-          {
-            RegNo: data.reg,
-          },
-          {
-            CourseCode: { in: codes },
-          },
-        ],
-      },
-      // include: {
-      //   CourseTitle: true,
-      // },
-    });
-    // console.log("result", result);
-
-    // let semesters = { };
-
-    let transcript = {
-      reg: data.reg,
-      name: student.Name,
-      faculty: student.Faculty,
-      batch: student.Batch,
-    };
-
-    if (result) {
-      // semesters.set(`${result.Semester}`, arr);
-      // trans.Semesters = semesters;
-      for (const obj of result) {
-        const course = await prisma.schemeofstudy.findFirst({
-          select: {
-            CourseTitle: true,
-            Semester: true,
-          },
-          where: {
-            CourseCode: obj.CourseCode,
-          },
+        const student = await prisma.student.findUnique({
+            where: {
+                RegNo: data.reg,
+            },
         });
-        // console.log(course);
-        obj["course_name"] = course.CourseTitle;
-        obj["number"] = course.Semester;
-      }
 
-      console.log("result", result.length);
+        const schemeCourses = await prisma.schemeofstudy.findMany({
+            where: {
+                Batch: student.batch,
+            },
+            select: {
+                Semester: true,
+                CourseCode: true,
+            },
+            orderBy: {
+                Semester: "asc",
+            },
+        });
 
-      result.forEach((obj) => {
-        // console.log(obj);
-        // for (const key in obj) {
+        let codes = [];
+        schemeCourses.forEach((course) => {
+            //   console.log(code);
+            codes.push(course.CourseCode);
+        });
 
-        // if (key == "Semester") {
-        // console.log(obj[key]);
-        const key = "Semester";
-        if (
-          !transcript.hasOwnProperty(`${obj["number"]}`)
-          // .hasOwnProperty(`${obj[key]}`)
-        ) {
-          // semesters.set(`${obj[key]}`, []);
-          // semesters[`${obj[key]}`] = [];
-          // semesters[`${obj[key]}`] = [];
-          transcript[`${obj["number"]}`] = {};
-          transcript[`${obj["number"]}`][`${obj[key]}`] = [];
-          // semesters[`${obj["number"]}_${obj[key]}`] = [];
+        let result = await prisma.courseplo.findMany({
+            select: {
+                Semester: true,
+                // CourseTitle: true,
+                CourseCode: true,
+                PLO1: true,
+                PLO2: true,
+                PLO3: true,
+                PLO4: true,
+                PLO5: true,
+                PLO6: true,
+                PLO7: true,
+                PLO8: true,
+                PLO9: true,
+                PLO10: true,
+                PLO11: true,
+                PLO12: true,
+            },
+            where: {
+                AND: [
+                    {
+                        RegNo: data.reg,
+                    },
+                    {
+                        CourseCode: {in: codes},
+                    },
+                ],
+            },
+            // include: {
+            //   CourseTitle: true,
+            // },
+        });
+        // console.log("result", result);
+
+        // let semesters = { };
+
+        result.forEach((obj) => {
+                let [semType, year] = obj.Semester.split(' ');
+                obj.semType = semesterLookup[semType];
+                obj.year = year;
+            }
+        );
+
+        let tmpResult = {};
+        for (let row of result){
+            if (!Object.keys(tmpResult).includes(row.year)){
+                tmpResult[row.year] = {};
+            }
+            if (!Object.keys(tmpResult[row.year]).some(el => el == row.semType)) {
+                tmpResult[row.year][row.semType] = [];
+            }
+            tmpResult[row.year][row.semType].push(row);
         }
-        let keys = [];
-        // storing keys from obj
-        keys = Object.keys(obj)
-          // removing semester key from array
-          .filter((k) => k != key);
 
-        // let values = new Map();
-        let values = {};
-        keys.forEach((i) => {
-          // values.set(i, obj[i]);
-          values[i] = obj[i];
-        });
+        let transcript = {
+            reg: data.reg,
+            name: student.Name,
+            faculty: student.Faculty,
+            batch: student.Batch,
+            result: tmpResult
+        };
 
-        // console.log(values);
-        // semesters.get(`${obj[key]}`).push(values);
-
-        // renaming CourseCode key to course_code
-        delete Object.assign(values, {
-          ["course_code"]: values["CourseCode"],
-        })["CourseCode"];
-
-        // semesters[`${obj[key]}`].push(values);
-        // semesters[`${obj["number"]}_${obj[key]}`].push(values);
-        // console.log(semesters[`${obj[key]}`]);
-
-        transcript[`${obj["number"]}`][`${obj[key]}`].push(values);
-        // }
-        // }
-        // console.log(semesters);
-        // console.log(JSON.parse(semesters));
-      });
+        res.send(JSON.stringify(transcript));
+    } catch (e) {
+        console.log(e);
+        res.status(404).send(JSON.stringify({message: e.toString()}));
     }
-    // let length = 0;
-    // Object.keys(transcript).forEach((key) => {
-    //   console.log(transcript[key]);
-    // });
-    // console.log("transcript", length);
-    res.send(JSON.stringify(transcript));
-    // res.send(JSON.stringify(result));
-  } catch (e) {
-    console.log(e);
-    res.status(404).send(JSON.stringify({ message: e.toString() }));
-  }
 });
 
 router.post("/add", async (req, res, next) => {
-  try {
-    console.log("route hit!");
-    // const title = req.body.title;
-    const files = req.files;
-    // req.files.forEach((file) =>
-    /*
-{
-        RegNo: 2201846,
-        CourseCode: 
-        Semester:
-        PLO1:
-        PLO2:
-        PLO3:
-        PLO4:
-        PLO5:
-        PLO6:
-        PLO7:
-        PLO8:
-        PLO9:
-        PLO10:
-        PLO11:
-        PLO12:
-      }
-    */
-    let data = [];
-    for (let i = 0; i < files.length; i++) {
-      const filename = files[i].originalname;
-      try {
-        let parsedfile = await require("../helper/parse").read(filename);
-        // console.log(parsedfile[0]["Reg No."]);
-        console.log("file:", filename);
-        console.log(parsedfile);
-        parsedfile.forEach((record) => {
-          data.push({
-            RegNo: parseInt(record["Reg No."]),
-            CourseCode: record["Course Code"],
-            Semester: record["Semester"],
-            PLO1: record["PLO 1"],
-            PLO2: record["PLO 2"],
-            PLO3: record["PLO 3"],
-            PLO4: record["PLO 4"],
-            PLO5: record["PLO 5"],
-            PLO6: record["PLO 6"],
-            PLO7: record["PLO 7"],
-            PLO8: record["PLO 8"],
-            PLO9: record["PLO 9"],
-            PLO10: record["PLO 10"],
-            PLO11: record["PLO 11"],
-            PLO12: record["PLO 12"],
-          });
+    try {
+        console.log("route hit!");
+        // const title = req.body.title;
+        const files = req.files;
+        // req.files.forEach((file) =>
+        /*
+    {
+            RegNo: 2201846,
+            CourseCode:
+            Semester:
+            PLO1:
+            PLO2:
+            PLO3:
+            PLO4:
+            PLO5:
+            PLO6:
+            PLO7:
+            PLO8:
+            PLO9:
+            PLO10:
+            PLO11:
+            PLO12:
+          }
+        */
+        let data = [];
+        for (let i = 0; i < files.length; i++) {
+            const filename = files[i].originalname;
+            try {
+                let parsedfile = await require("../helper/parse").read(filename);
+                // console.log(parsedfile[0]["Reg No."]);
+                console.log("file:", filename);
+                console.log(parsedfile);
+                parsedfile.forEach((record) => {
+                    data.push({
+                        RegNo: parseInt(record["Reg No."]),
+                        CourseCode: record["Course Code"],
+                        Semester: record["Semester"],
+                        PLO1: record["PLO 1"],
+                        PLO2: record["PLO 2"],
+                        PLO3: record["PLO 3"],
+                        PLO4: record["PLO 4"],
+                        PLO5: record["PLO 5"],
+                        PLO6: record["PLO 6"],
+                        PLO7: record["PLO 7"],
+                        PLO8: record["PLO 8"],
+                        PLO9: record["PLO 9"],
+                        PLO10: record["PLO 10"],
+                        PLO11: record["PLO 11"],
+                        PLO12: record["PLO 12"],
+                    });
+                });
+            } catch (e) {
+                console.log(e);
+                res.status(404).send(e.toString);
+            }
+        }
+
+        let response = await prisma.courseplo.createMany({
+            data: data,
         });
-      } catch (e) {
+        console.log("response", response);
+        res.sendStatus(200);
+    } catch (e) {
         console.log(e);
         res.status(404).send(e.toString);
-      }
     }
-
-    let response = await prisma.courseplo.createMany({
-      data: data,
-    });
-    console.log("response", response);
-    res.sendStatus(200);
-  } catch (e) {
-    console.log(e);
-    res.status(404).send(e.toString);
-  }
 });
 
 module.exports = router;
